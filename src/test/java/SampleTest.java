1ba393127b0954d72da30381927a84c841c2331d
Ted3264 <teddy.moreau.edu@gmail.com>
2020-07-01 12:53:57 +0200

    C2

--- src/test/java/SampleTest.java
+++ src/test/java/SampleTest.java
@@ -1,9 +1,8 @@
-package MyCompany.JUnit;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.api.Test;
 class SampleTest {
 	@Test
 	void test01() {
-		assertEquals("abc","abc");
+		assertEquals("abc123","abc123");
 	}
 }
